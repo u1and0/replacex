a8923f528cfa6a9316258b9f8d6e575b7a7a56d4
u1and0 <e01.ando60@gmail.com>
2021-08-02 17:59:07 +0900

    [mod] < old new filename > for single file changes

--- xreplace.py
+++ xreplace.py
@@ -1,7 +1,8 @@
 """
 docxファイルの文字列置換を行うモジュール
 usage:
-    $ python xreplace.py oldfile.docx newfile.docx replaceword.csv
+    $ python xreplace.py oldword newword filename
+    $ python xreplace.py -f oldfile.docx -o newfile.docx -w replaceword.csv
 """
 import sys
 import csv
@@ -16,34 +17,40 @@ def replace_text(paragraph, before, after):
     if paragraph.text != replaced_text:
         paragraph.text = replaced_text
         # paragraph.runs[0].font.size = Pt(12)
-        paragraph.runs[0].font.color.rgb = RGBColor(204, 0, 0)
+        paragraph.runs[0].font.color.rgb = RGBColor(235, 0, 0)
 
 
 def replace_texts(readfile, writefile, *words):
     """
     readfile内の本文とテーブルの文字列を
-    REPLACERに従って置換して
+    wordsに従って置換して
     writefileへ保存する。
     """
     document = Document(readfile)
     for word in words:
         # 本文書き換え
         for paragraph in document.paragraphs:
-            replace_text(paragraph, *word)
+            replace_text(paragraph, word[0], word[1])
         # テーブル書き換え
         paragraphs = (paragraph for table in document.tables
                       for row in table.rows for cell in row.cells
                       for paragraph in cell.paragraphs)
         for paragraph in paragraphs:
-            replace_text(paragraph, *word)
+            replace_text(paragraph, word[0], word[1])
     document.save(writefile)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         raise ValueError('引数が足りません' + __doc__)
-    oldfile, newfile = sys.argv[1], sys.argv[2]
-    with open(sys.argv[3], newline='', encoding='sjis') as csvfile:
-        reader = csv.reader(csvfile, delimiter=(','), quotechar='|')
-        replacer = [row[0].split('\t') for row in reader]
-    replace_texts(oldfile, newfile, *replacer)
+    old, new, filename = sys.argv[1], sys.argv[2], sys.argv[3]
+    document = Document(filename)
+    # 本文書き換え
+    for paragraph in document.paragraphs:
+        replace_text(paragraph, old, new)
+    # テーブル書き換え
+    paragraphs = (paragraph for table in document.tables for row in table.rows
+                  for cell in row.cells for paragraph in cell.paragraphs)
+    for paragraph in paragraphs:
+        replace_text(paragraph, old, new)
+    document.save(filename)
