f7b0ea1a25dd224c33f6778a420965cfa8ed03cc
u1and0 <e01.ando60@gmail.com>
2021-08-03 11:20:33 +0900

    [mod] multiple filename replace

--- renamex.py
+++ renamex.py
@@ -5,7 +5,6 @@ usage:
 """
 import sys
 from docx import Document
-# from docx.shared import Pt
 from docx.shared import RGBColor
 
 
@@ -14,7 +13,6 @@ def replace_text(paragraph, before, after):
     replaced_text = paragraph.text.replace(before, after)
     if paragraph.text != replaced_text:
         paragraph.text = replaced_text
-        # paragraph.runs[0].font.size = Pt(12)
         paragraph.runs[0].font.color.rgb = RGBColor(235, 0, 0)
 
 
@@ -38,17 +36,21 @@ def replace_texts(readfile, writefile, *words):
     document.save(writefile)
 
 
+def main(old, new, *filenames):
+    """引数に対してreplace_textを実行する"""
+    for filename in filenames:
+        document = Document(filename)
+        # 本文書き換え
+        for paragraph in document.paragraphs:
+            replace_text(paragraph, old, new)
+        # テーブル書き換え
+        paragraphs = (paragraph for table in document.tables
+                      for row in table.rows for cell in row.cells
+                      for paragraph in cell.paragraphs)
+        for paragraph in paragraphs:
+            replace_text(paragraph, old, new)
+        document.save(filename)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        raise ValueError('引数が足りません' + __doc__)
-    old, new, filename = sys.argv[1], sys.argv[2], sys.argv[3]
-    document = Document(filename)
-    # 本文書き換え
-    for paragraph in document.paragraphs:
-        replace_text(paragraph, old, new)
-    # テーブル書き換え
-    paragraphs = (paragraph for table in document.tables for row in table.rows
-                  for cell in row.cells for paragraph in cell.paragraphs)
-    for paragraph in paragraphs:
-        replace_text(paragraph, old, new)
-    document.save(filename)
+    main(sys.argv[1], sys.argv[2], *sys.argv[3:])
