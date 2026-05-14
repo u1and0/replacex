af170ba7ea34e6abb3bb9ca6b81c7f7d6bfd29d4
u1and0 <e01.ando60@gmail.com>
2026-05-14 10:34:17 +0900

    [chore] uv tool install . でどこでも使えるようにディレクトリ構成変更

--- src/replacex/main.py
+++ src/replacex/main.py
@@ -5,6 +5,7 @@
 
 $ pip install python-docx
 """
+
 import sys
 import argparse
 from itertools import chain
@@ -12,9 +13,9 @@ from more_itertools import collapse
 from docx import Document
 from docx.shared import RGBColor
 
-VERSION = 'v1.0.2'
-CRED = '\033[91m'
-CEND = '\033[0m'
+VERSION = "v1.0.2"
+CRED = "\033[91m"
+CEND = "\033[0m"
 
 
 def replace_text(paragraph, before, after):
@@ -31,16 +32,20 @@ def replace_document(old, new, document):
     # Rewrite sentence
     sentence_paragraphs = (paragraph for paragraph in document.paragraphs)
     # Rewrite table
-    table_paragraphs = (paragraph for table in document.tables
-                        for row in table.rows for cell in row.cells
-                        for paragraph in cell.paragraphs)
+    table_paragraphs = (
+        paragraph
+        for table in document.tables
+        for row in table.rows
+        for cell in row.cells
+        for paragraph in cell.paragraphs
+    )
     # Concat iter
     paragraphs = chain(sentence_paragraphs, table_paragraphs)
     # Edit contents
     return (replace_text(paragraph, old, new) for paragraph in paragraphs)
 
 
-def main(old, new, *filenames, dryrun, verbose):
+def run(old, new, *filenames, dryrun, verbose):
     """execute replace_document to multiple files"""
     for filename in filenames:
         document = Document(filename)
@@ -63,37 +68,37 @@ def main(old, new, *filenames, dryrun, verbose):
 def parse():
     """arg parser"""
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('old', type=str, help='old word')
-    parser.add_argument('new', type=str, help='new word')
-    parser.add_argument('files', type=str, nargs='*', help='docx file path')
+    parser.add_argument("old", type=str, help="old word")
+    parser.add_argument("new", type=str, help="new word")
+    parser.add_argument("files", type=str, nargs="*", help="docx file path")
     parser.add_argument(
-        '-n',
-        '--dryrun',
-        help='DO NOT save docx file just print replacement result.',
-        action='store_true',
+        "-n",
+        "--dryrun",
+        help="DO NOT save docx file just print replacement result.",
+        action="store_true",
         default=False,
     )
     parser.add_argument(
-        '-v',
-        '--verbose',
-        help='print replacement result to stdout',
-        action='store_true',
+        "-v",
+        "--verbose",
+        help="print replacement result to stdout",
+        action="store_true",
         default=False,
     )
-    parser.add_argument('-V', '--version', action='store_true')
+    parser.add_argument("-V", "--version", action="store_true")
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     # perse()内だとPositional Argument として
     # old, newを要求するからversion表示がされないので、
     # ここであえてversion表示スクリプトをparse()前に走らせる
-    if '-V' in sys.argv or '--version' in sys.argv:
-        print('replacex:', VERSION)
+    if "-V" in sys.argv or "--version" in sys.argv:
+        print("replacex:", VERSION)
         sys.exit(0)
     argv = parse()
-    main(argv.old,
-         argv.new,
-         *argv.files,
-         dryrun=argv.dryrun,
-         verbose=argv.verbose)
+    run(argv.old, argv.new, *argv.files, dryrun=argv.dryrun, verbose=argv.verbose)
+
+
+if __name__ == "__main__":
+    main()
